89f182b450e7172a5c0545cffd8e6e7a6408a621
Chirag Tyagi <tyagichirag06@gmail.com>
2023-07-23 04:26:14 +0530

    added views and urls

--- comicvenger/comictextmaker/urls.py
+++ comicvenger/comictextmaker/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     
     path('', views.index, name = 'index'),
     path('home/',views.home, name = 'home'),
+    path('yourcomic/', views.story, name = 'yourcomic'),
+    path('about/', views.about, name = 'about'),
 
 ]
--- comicvenger/comictextmaker/views.py
+++ comicvenger/comictextmaker/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import random
 
 # Create your views here.
 
@@ -9,3 +10,27 @@ def index(request):
 
 def home(request):
     return render(request, 'comictextmaker/home.html')
+
+def story(request):
+    # List of background images
+    background_images = [
+        'comictextmaker/captain_america.jpg',
+        'comictextmaker/evil.jpg',
+        'comictextmaker/iron_man.jpg',
+        'comictextmaker/meet.jpg',
+        'comictextmaker/office.jpg',
+        'comictextmaker/plane.jpg',
+        'comictextmaker/spaceship.jpg',
+        'comictextmaker/spider_man.jpg',
+        'comictextmaker/stark.jpg',
+        'comictextmaker/thanos.jpg',
+        # Add more background images as needed
+    ]
+
+    # Select a random image from the list
+    random_image = random.choice(background_images)
+
+    return render(request, 'comictextmaker/story.html', {'random_image': random_image})
+
+def about(request):
+    return render(request, 'comictextmaker/about.html')
